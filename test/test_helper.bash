96ff566c0c62562967f7066d6ae2a358701a2784
Benjamin Wuethrich <benjamin.wuethrich@gmail.com>
2024-01-13 18:49:27 -0500

    Add test helpers

---
+++ test/test_helper.bash
@@ -0,0 +1,21 @@
+setup() {
+	# shellcheck source=../tagupdater
+	source tagupdater
+
+	local repo='/tmp/tagupdater-testrepo'
+	mkdir --parents "$repo"
+	git -C "$repo" init --quiet
+
+	if [[ -z $(git -C "$repo" config --get user.name) ]]; then
+		git -C "$repo" config user.name "Integration Test"
+	fi
+	if [[ -z $(git -C "$repo" config --get user.email) ]]; then
+		git -C "$repo" config user.email "integration.test@example.com"
+	fi
+
+	cd /tmp/tagupdater-testrepo || return 1
+}
+
+teardown() {
+	rm -rf /tmp/tagupdater-testrepo
+}
